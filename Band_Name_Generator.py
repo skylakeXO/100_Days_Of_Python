332918fd1bd30fc56e6897db70850f9c73c655e5
Mary Kay <60500030+skylakeXO@users.noreply.github.com>
2022-04-10 16:49:28 -0700

    Create Band_Name_Generator.py

---
+++ Band_Name_Generator.py
@@ -0,0 +1,6 @@
+print('Welcome to Band Name Generator! \n')
+
+user_city = input('What is the name of the city you grew up in? \n')
+user_pet = input('What is the name of your pet? \n')
+
+print('Your cool band name is ' + user_city + ' ' + user_pet + '.')
